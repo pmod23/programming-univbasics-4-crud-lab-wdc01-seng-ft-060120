652e5fcecb2db4da2b7f6415f71f83574b31347b
Pramod Thomas <pmod23@gmail.com>
2020-05-21 14:24:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,12 @@
 def create_an_empty_array
   []
+  
 end
 
 def create_an_array
-  [] <<
+  arr = {"hello", "good morning", "goodbye", "good afternoon"}
+  arr
+  
 end
 
 def add_element_to_end_of_array(array, element)
